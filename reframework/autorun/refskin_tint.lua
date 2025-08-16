ec03ba13f77467a6feef6423f4c45e33d11395de
Rox Cian <40283094+RoxCian@users.noreply.github.com>
2025-08-17 01:41:39 +0800

    Rewrite the loading procedure. Add a mechanism of failure penalty. Use a new hook to handle equipment update.  Reduce the frequency of processing in skin color edit window.

--- reframework/autorun/refskin_tint.lua
+++ reframework/autorun/refskin_tint.lua
@@ -11,8 +11,11 @@ local loading_gui
 local players = {}
 local npcs = {}
 local npcs_id = {}
+local max_failed_count = 20
 local player_failed_flag = 0
 local npc_failed_flag = 0
+local player_failed_cycles_skip = 0
+local npc_failed_cycles_skip = 0
 local frame_counter = 0
 local is_exec_asap = false
 local is_dirty = true
@@ -22,6 +25,7 @@ local is_title_scene = false
 local is_hunter_only_scene = false
 local hunter_only_scene_key = nil
 local continuously_exec_frame_count = -1
+local request_update = false
 local is_null_or_empty = M("System.String", "IsNullOrEmpty(System.String)")
 
 -- Entry
@@ -119,7 +123,6 @@ local function tint_skin_tone_game_object(game_object, skin_tone_vec)
         local mat_param = mesh:getMaterialVariableNum(j)
 
         if mat_name and mat_param and string.sub(mat_name, 1, 8) == "REFSKIN_" then
-            
             for k = 0, mat_param - 1 do
                 local mat_param_name = mesh:getMaterialVariableName(j, k)
                 local mat_param_type = mesh:getMaterialVariableType(j, k)
@@ -207,15 +210,6 @@ local function dump_npcs()
     return true
 end
 
--- Loading state
-local function loading_state_update()
-    if not is_loading then return end
-    if loading_gui and loading_gui._Param:getValue() == 90.0 then return end
-    is_dirty = true
-    is_loading = false
-    loading_gui = nil
-end
-
 -- Executor
 local function exec_title_scene()
     for i = 0, 1 do
@@ -234,62 +228,85 @@ local function exec_hunter_only_scene()
     return pl000 == nil or tint_skin_tone(pl000)
 end
 
+
 local function exec()
-    loading_state_update()
     if (not is_dirty and not is_dirty_npc) or is_loading then return false end
+    local skip_player = player_failed_cycles_skip > 0
+    local skip_npc = npc_failed_cycles_skip > 0
+
     local pl000_succeeded = false
     local player_succeeded = false
     local npc_succeeded = false
-    if is_dirty then
-        if is_title_scene and not is_hunter_only_scene then
-            if not exec_title_scene() then return false end
-            player_succeeded = true
-        elseif is_hunter_only_scene then
-            if not exec_hunter_only_scene() then return false end
-            player_succeeded = true
-        else
-            local pl000 = find_pl000(1)
-            if pl000 then
-                pl000_succeeded = tint_skin_tone(pl000)
-            end
 
-            if dump_players() then
-                player_succeeded = true
-                for i = #players, 1, -1 do
-                    if players[i] == nil or not players[i]:get_Valid() or tint_skin_tone(players[i]) then table.remove(players, i) end
+    -- worker
+    if is_dirty then
+        if skip_player then
+            player_failed_cycles_skip = player_failed_cycles_skip - 1
+        else
+            if is_title_scene and not is_hunter_only_scene then
+                player_succeeded = exec_title_scene()
+            elseif is_hunter_only_scene then
+                player_succeeded = exec_hunter_only_scene()
+            else
+                local pl000 = find_pl000(1)
+                if pl000 then
+                    pl000_succeeded = tint_skin_tone(pl000)
                 end
-                player_succeeded = #players == 0
+
+                if dump_players() then
+                    player_succeeded = true
+                    for i = #players, 1, -1 do
+                        if players[i] == nil or not players[i]:get_Valid() or tint_skin_tone(players[i]) then table.remove(players, i) end
+                    end
+                    player_succeeded = #players == 0
+                end
             end
         end
     end
     if is_dirty_npc then
-        npc_succeeded = dump_npcs()
-        for i = #npcs, 1, -1 do
-            if npcs[i] == nil or tint_skin_tone(npcs[i], nil, true) then table.remove(npcs, i) end
-        end
-        npc_succeeded = npc_succeeded and #npcs == 0 
-    end
-    if pl000_succeeded or player_succeeded then
-        player_failed_flag = 0
-        is_dirty = false
-    else
-        player_failed_flag = player_failed_flag + 1
-        if player_failed_flag > 12 then
-            player_failed_flag = 0
-            is_dirty = false
+        if skip_npc then
+            npc_failed_cycles_skip = npc_failed_cycles_skip - 1
+        else
+            npc_succeeded = dump_npcs()
+            for i = #npcs, 1, -1 do
+                if npcs[i] == nil or tint_skin_tone(npcs[i], nil, true) then table.remove(npcs, i) end
+            end
+            npc_succeeded = npc_succeeded and #npcs == 0
         end
     end
 
-    if npc_succeeded then
-        npc_failed_flag = 0
-        is_dirty_npc = false
-    else
-        npc_failed_flag = npc_failed_flag + 1
-        if npc_failed_flag > 12 then
-            npc_failed_flag = 0
-            is_dirty_npc = false
+    -- checker
+    if not skip_player then
+        if pl000_succeeded or player_succeeded then
+            player_failed_flag = 0
+            is_dirty = false
+        else
+            player_failed_flag = player_failed_flag + 1
+            if player_failed_flag > max_failed_count then
+                player_failed_flag = 0
+                is_dirty = false
+            elseif player_failed_flag | 1 ~= player_failed_flag then
+                -- penalty for every 2 failures
+                player_failed_cycles_skip = player_failed_flag
+            end
         end
     end
+    if not skip_npc then
+        if npc_succeeded then
+            npc_failed_flag = 0
+            is_dirty_npc = false
+        else
+            npc_failed_flag = npc_failed_flag + 1
+            if npc_failed_flag > max_failed_count then
+                npc_failed_flag = 0
+                is_dirty_npc = false
+            elseif npc_failed_flag | 1 ~= npc_failed_flag then
+                -- penalty for every 2 failures
+                npc_failed_cycles_skip = npc_failed_flag
+            end
+        end
+    end
+
     return pl000_succeeded or player_succeeded or npc_succeeded
 end
 
@@ -299,6 +316,16 @@ H(M("app.GUI010000", "onOpen()"),
     function(args)
         loading_gui = sdk.to_managed_object(args[2])
         is_loading = true
+    end
+)
+H(M("app.GUIBaseApp", "onCloseApp()"),
+    function(args)
+        if is_loading then
+            local this = sdk.to_managed_object(args[2])
+            local type_name = this and this:GetType():get_FullName()
+            if type_name ~= "app.GUI010000" then return end
+            is_loading = false
+        end
     end
 )
 -- -- Title scene
@@ -322,29 +349,31 @@ H(M("app.GUI080000", "updateCurrentEquipData(app.EquipDef.EquipSet[], System.Boo
     noop,
     function(result)
         mark_dirty()
-        -- It has delay between updateEquipData() executed and selected armor shown in equipment edit scene 
-        -- to make animation works and I cannot locate the function of show newly selected armor.
-        -- Make it work continuously for 3 cycles for now.
-        continuously_exec_frame_count = 32
+        request_update = true
     end
 )
 H(M("app.GUI080000", "updateCurrentEquipData(app.EquipDef.EquipSet, System.Boolean)"),
     noop,
     function(result)
-        mark_dirty()
-        continuously_exec_frame_count = 32
+        request_update = true
     end
 )
 H(M("app.GUI080000", "onClose()"),
     noop,
     mark_dirty_hook
 )
+H(M("app.mcDelayedCarry", "addItem(via.GameObject, via.GameObject, app.user_data.DelayedCarryData)"),
+    noop,
+    function(result)
+        if request_update then mark_dirty(true) end
+        request_update = false
+    end
+)
 -- -- Equipment select scene (tent)
 H(M("app.GUI080001", "updateEquipParts(app.EquipDef.EQUIP_INDEX)"),
     noop,
     function(result)
-        mark_dirty()
-        continuously_exec_frame_count = 32
+        request_update = true
     end
 )
 H(M("app.GUI080001", "onClose()"),
@@ -355,15 +384,19 @@ H(M("app.GUI080001", "onClose()"),
 H(M("app.GUI080200HunterOverview", "callbackDecide(via.gui.Control, via.gui.SelectItem, System.UInt32)"),
     noop,
     function(result)
-        mark_dirty()
-        continuously_exec_frame_count = 32
+        request_update = true
     end
 )
 H(M("app.GUI080200HunterOverview", "updateValuesCore()"),
     noop,
     function(result)
-        mark_dirty()
-        continuously_exec_frame_count = 32
+        request_update = true
+    end
+)
+H(M("app.GUI080200HunterOverview", "applyMyset(via.gui.SelectItem)"),
+    noop,
+    function(result)
+        request_update = true
     end
 )
 -- -- Hunter edit scene
@@ -379,7 +412,7 @@ H(M("app.CharaMakeSceneController", "onDestroy()"),
         leave_hunter_only_scene("CharaMake", false)
     end
 )
-H(M("app.characteredit.protagonist.cUnderwear", "apply(app.characteredit.CharacterEditContext)"),
+H(M("app.characteredit.protagonist.cUnderwear", "applyCore(app.characteredit.CharacterEditContext)"),
     noop,
     function(result)
         if is_hunter_only_scene and hunter_only_scene_key == "CharaMake" then
@@ -389,14 +422,18 @@ H(M("app.characteredit.protagonist.cUnderwear", "apply(app.characteredit.Charact
 )
 H(M("app.characteredit.basic.cSkin.Editor", "set_ColorU(System.Single)"),
     function(args)
-        if is_hunter_only_scene and hunter_only_scene_key == "CharaMake" then
+        if is_hunter_only_scene and hunter_only_scene_key == "CharaMake" and not is_dirty then
+            local skin_editor = sdk.to_managed_object(args[2])
+            if sdk.to_float(args[3]) == skin_editor:get_ColorU() then return end
             mark_dirty(true)
         end
     end
 )
 H(M("app.characteredit.basic.cSkin.Editor", "set_ColorV(System.Single)"),
     function(args)
-        if is_hunter_only_scene and hunter_only_scene_key == "CharaMake" then
+        if is_hunter_only_scene and hunter_only_scene_key == "CharaMake" and not is_dirty then
+            local skin_editor = sdk.to_managed_object(args[2])
+            if sdk.to_float(args[3]) == skin_editor:get_ColorV() then return end
             mark_dirty(true)
         end
     end
@@ -408,10 +445,10 @@ H(M("app.SaveSelectSceneController", "doStart()"),
         enter_hunter_only_scene("SaveSelect", false)
     end
 )
-H(M("app.SaveSelectSceneController", "update()"),
+H(M("app.SaveSelectSceneController", "displaySaveCharacter()"),
     noop,
     function(result)
-        enter_hunter_only_scene("SaveSelect", true)
+        enter_hunter_only_scene("SaveSelect", false)
     end
 )
 H(M("app.SaveSelectSceneController", "doOnDestroy()"),
@@ -457,10 +494,11 @@ H(M("app.PlayerManager", "bindGameObject(via.GameObject, app.cPlayerContextHolde
 --
 re.on_frame(function()
     if frame_counter == 0 or is_exec_asap or continuously_exec_frame_count >= 0 then exec() end
-    frame_counter = math.fmod(frame_counter + 1, 16)
+    frame_counter = frame_counter + 1
+    if frame_counter >= 30 then frame_counter = 0 end
     if continuously_exec_frame_count >= 0 then
         continuously_exec_frame_count = continuously_exec_frame_count - 1
-        if continuously_exec_frame_count >= 0 and math.fmod(continuously_exec_frame_count, 16) == 0 then mark_dirty() end
+        if continuously_exec_frame_count >= 0 and math.fmod(continuously_exec_frame_count, 30) == 0 then mark_dirty() end
     end
     is_exec_asap = false
 end)
